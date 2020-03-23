d5480589e261fffcd758acdb0ea477750e6b07fb
saumjohn97 <saumjohn97@penguin.lxd>
2020-03-23 21:29:51 +0000

    final changes to the website

--- js/map.js
+++ js/map.js
@@ -31,80 +31,80 @@ var markers = [
     },
     {
         "lat": 51.5139289,
-        "lng":-0.1337045,
+        "lng": -0.1337045,
         "title": "Chotto Matte",
         "stars": 3
     },
     {
         "lat": 51.5224915,
-        "lng":-0.0825905,
-        "title":"Quaker Street Cafe",
+        "lng": -0.0825905,
+        "title": "Quaker Street Cafe",
         "stars": 1
     },
     {
-        "lat":51.5163688,
-        "lng":-0.0785026,
-        "title":"Petticoat Lane",
+        "lat": 51.5163688,
+        "lng": -0.0785026,
+        "title": "Petticoat Lane",
         "stars": 2
     },
     {
-        "lat":51.5256852,
-        "lng":-0.0826429,
-        "title":"Ballie Ballerson",
+        "lat": 51.5256852,
+        "lng": -0.0826429,
+        "title": "Ballie Ballerson",
         "stars": 1
     },
     {
-        "lat":51.5245028,
-        "lng":-0.073942,
-        "title":"Biegel Bake",
+        "lat": 51.5245028,
+        "lng": -0.073942,
+        "title": "Biegel Bake",
         "stars": 1
     },
     {
-        "lat":51.5208912,
-        "lng":-0.0799096,
-        "title":"Dennis Server",
+        "lat": 51.5208912,
+        "lng": -0.0799096,
+        "title": "Dennis Server",
         "stars": 1
     },
     {
-        "lat":51.5239206,
-        "lng":-0.0738005,
-        "title":"Cereal Killer Cafe",
+        "lat": 51.5239206,
+        "lng": -0.0738005,
+        "title": "Cereal Killer Cafe",
         "stars": 2
     },
     {
-        "lat":51.5124824,
-        "lng":-0.1407383,
-        "title":"Cahoots",
+        "lat": 51.5124824,
+        "lng": -0.1407383,
+        "title": "Cahoots",
         "stars": 3
     },
     {
-        "lat":51.5250456,
-        "lng":-0.0850524,
-        "title":"Nami-Nobu Hotel",
+        "lat": 51.5250456,
+        "lng": -0.0850524,
+        "title": "Nami-Nobu Hotel",
         "stars": 4
     },
     {
-        "lat":51.5265294,
-        "lng":-0.0899648,
-        "title":"Nightjar",
+        "lat": 51.5265294,
+        "lng": -0.0899648,
+        "title": "Nightjar",
         "stars": 3
     },
     {
-        "lat":51.5262938,
-        "lng":-0.0820903,
-        "title":"Callooh Callay",
+        "lat": 51.5262938,
+        "lng": -0.0820903,
+        "title": "Callooh Callay",
         "stars": 2
     },
     {
-        "lat":51.5150573,
-        "lng":-0.0883021,
-        "title":"Demon,Wise & Partner",
+        "lat": 51.5150573,
+        "lng": -0.0883021,
+        "title": "Demon,Wise & Partner",
         "stars": 3
     },
     {
-        "lat":51.5243756,
-        "lng":-0.0745705,
-        "title":"The Cocktail Trading Co",
+        "lat": 51.5243756,
+        "lng": -0.0745705,
+        "title": "The Cocktail Trading Co",
         "stars": 2
     }
 ]
@@ -112,7 +112,7 @@ var markers = [
 function createMarkerInfoString(marker_info) {
     num_stars = marker_info['stars'];
     title_of_place = marker_info['title'];
-    var info_string = '<h3>'  +
+    var info_string = '<h3>' +
         title_of_place +
         '</h3>' + '<span style="font-weight:bold">&#163;</span>'.repeat(num_stars);
     return info_string
@@ -121,10 +121,10 @@ function createMarkerInfoString(marker_info) {
 // Initialize and add the map
 function initMap() {
     // The location of shoreditch
-    var shoreditch_location = { lat: 51.5244122, lng: -0.0838806 };
+    var shoreditch_location = { lat: 51.5244484, lng: -0.0800882 };
     var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 14, center: shoreditch_location}
-          );
+        document.getElementById('map'), { zoom: 16, center: shoreditch_location }
+    );
     // Add all the markers to the map
     markers.forEach(function (marker_info, index) {
         var marker = new google.maps.Marker({
@@ -139,7 +139,7 @@ function initMap() {
             content: contentString
         });
         infowindow.open(map, marker);
-        
-    
+
+
     });
 }
